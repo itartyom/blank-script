a437d509577df677ef8ebe4830b693467e6f9316
arty <itartyom01@gmail.com>
2025-08-17 13:39:40 +0700

    base_dir, config variables and main function added

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import tomllib
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent.parent
+config = tomllib.loads((base_dir / "config.toml").read_text())
+
+def main():
+    print("Hello World")
+
 if __name__ == '__main__':
     main()
